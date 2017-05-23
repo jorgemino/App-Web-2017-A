96b0acdd78fe7a676939864a74a25a7c5efb799e
jorgemino <jorge.mino@epn.edu.ec>
2017-05-22 20:53:36 -0500

    variable

---
+++ 04-Javascript/funcion.js
@@ -0,0 +1,24 @@
+/**
+ * Created by USRDEL on 22/5/17.
+ */
+
+
+function Imprimir() {
+    console.log("Hola mundo");
+
+}
+Imprimir();
+function Suma2Numeros(a,b) {
+    return a+b;
+}
+
+var resultado=Suma2Numeros(1,2);
+console.log(resultado);
+console.log(Suma2Numeros(1,2));
+
+[1,2,3,4].forEach(function (valorIteration,Indice,Arreglo, p3) {  })
+
+
+sumar:function(a,b) {
+    return a + b;
+}
---
+++ 04-Javascript/loops.js
@@ -0,0 +1,5 @@
+/**
+ * Created by USRDEL on 22/5/17.
+ */
+
+for()
